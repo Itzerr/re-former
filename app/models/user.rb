f51ac2b850ceac33765a46e773acef5894dc7b46
Itzerr <eatzer@gmail.com>
2021-09-03 20:54:02 +0200

    created user edit view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true
+  validates :username, presence: true, length: { maximum: 30 }
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { in: 6..50 }
 end
